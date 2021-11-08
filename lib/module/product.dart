0d6d926ac512325c2a9bd122159aff81ffb756e7
Somesh <someshsa9860@gmail.com>
2021-11-08 21:33:21 +0530

    initial changes

---
+++ lib/module/product.dart
@@ -0,0 +1,17 @@
+import 'package:/flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class _productnit {
+  final String image, title, discription;
+  final int price, size, id;
+  final Color color;
+
+  _productnit(
+      {this.image,
+      this.title,
+      this.discription,
+      this.price,
+      this.size,
+      this.id,
+      this.color});
+}
